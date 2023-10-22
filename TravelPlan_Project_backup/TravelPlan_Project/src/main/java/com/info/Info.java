95bd348642c7e5aee539f3cd7722663411792a80
Hyuk Min Yang <hmtank2@gmail.com>
2023-10-23 05:32:45 +0900

    10/20 풀 받은 내용 커밋 전 백업

---
+++ TravelPlan_Project_backup/TravelPlan_Project/src/main/java/com/info/Info.java
@@ -0,0 +1,19 @@
+package com.info;
+
+public class Info {
+	private String mailId = "pjtravelplan";
+	private String mailPassword = "jckk ycis dyts fzno";
+	
+	public String getMailId() {
+		return mailId;
+	}
+	public void setMailId(String mailId) {
+		this.mailId = mailId;
+	}
+	public String getMailPassword() {
+		return mailPassword;
+	}
+	public void setMailPassword(String mailPassword) {
+		this.mailPassword = mailPassword;
+	}
+}
