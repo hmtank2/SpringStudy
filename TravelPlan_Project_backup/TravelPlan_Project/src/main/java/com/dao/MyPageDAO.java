d0c090f29fc3432a2e33d1b71b7fcedd684ebc40
Hyuk Min Yang <hmtank2@gmail.com>
2023-10-25 09:25:42 +0900

    10/24 변경사항 10/25 풀 전 백업

--- TravelPlan_Project_backup/TravelPlan_Project/src/main/java/com/dao/MyPageDAO.java
+++ TravelPlan_Project_backup/TravelPlan_Project/src/main/java/com/dao/MyPageDAO.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.dto.BoardDTO;
 import com.dto.MemberDTO;
 import com.dto.PlanDTO;
 import com.dto.TravelDTO;
@@ -13,18 +14,18 @@ import com.dto.UserLikeDTO;
 
 @Repository
 public class MyPageDAO {
-	
+	//, 일정보관함 plandto 맞나? 아님 나중에 수정하기
 	@Autowired
 	SqlSessionTemplate session;    //session 주입받기
 	
-	//수정할 회원 정보 가져오기
+	//회원 정보 확인
 	//userID에 해당하는 값들을 다뽑아내서 dto에 담고 반환
 	public MemberDTO memberInfo(String userID) {
 		MemberDTO dto = session.selectOne("MyPageMapper.memberInfo", userID);
 		return dto;
 	}
 	
-	//회원정보수정
+	//회원 정보 수정
 	//dto에 담긴 파라미터들을 MyPageMapper의 아이디가 memberUpdate인 쿼리에 넣어준다.
 	public void memberUpdate(MemberDTO dto) {
 		session.update("MyPageMapper.memberUpdate", dto);
@@ -41,8 +42,10 @@ public class MyPageDAO {
 	}
 
 	//여행계획 (내보관함) 목록
-	public List<TravelDTO> writeList(String userID) {
+	//10 23내가 쓴 글 목록
+	public List<BoardDTO> writeList(String userID) {
 		return session.selectList("MyPageMapper.writeList", userID);
 	}
+	
 	
 }
